d80275f41303bce4c472d744f93f413a40a28990
wingkeet <leongwingkeet@gmail.com>
2020-01-28 17:17:49 +0800

    Use fat arrow instead of function callback when calling ports.map()

--- rsinitiate.js
+++ rsinitiate.js
@@ -1,7 +1,7 @@
 // Call rs.initiate() only upon fresh install
 if (rs.status().ok === 0) {
     const host = hostname()
-    const members = ports.map(function(port, index) { return { _id: index, host: `${host}:${port}` } })
+    const members = ports.map((port, index) => ({ _id: index, host: `${host}:${port}` }) )
     members[0].priority = 10
     const rsconf = {
         _id: rsname,
